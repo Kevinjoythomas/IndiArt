4f49b7428104c0d5c69172425de8626423bbdd70
aayushpotdar <potdaraayush13@gmail.com>
2024-02-16 23:34:56 +0530

    adding state routes

--- src/App.js
+++ src/App.js
@@ -8,68 +8,29 @@ import {BrowserRouter, Route, Routes, Link, NavLink} from 'react-router-dom'
 import Contact from './Components/Contact/Contact';
 import AP from './Components/States/AP/AP'
 import Karnataka from './Components/States/Karnataka/Karnataka'
+import Kerela from './Components/States/Kerela/Kerala'
+import Maharashtra from './Components/States/Maharashtra/Maharashtra'
+import Odisha from './Components/States/Odisha/Odisha'
+import Rajasthan from './Components/States/Rajasthan/Rajasthan'
+import TamilNadu from './Components/States/TamilNadu/TamilNadu'
 function App() {
-  //doesnt work
-  // const router = createBrowserRouter (
-  //   createRoutesFromElements(
-  //     <Route path="/" element={<Root/>}>
-  //       <Route index element={<Navbar/>} />
-  //       <Route path="/About" element={<About/>}/>
-  //     </Route>
-  //   )
-  // )
   const stateNames=["AP", "Karnataka", "Kerela", "Maharashtra", "Odisha", "Rajasthan", "TamilNadu"]
 
   return (
     <div className="App">
-{/* 
-      <Router>
-        <Routes>
-          <Route path="/" element={<Root/>}>
-            <Route element={<Navbar/>} />
-            <Route path="about" element={<About/>}/>
-          </Route>
-        </Routes>
-      </Router> */}
-
-      {/* <RouterProvider route={router}/> */}
-
-
-      {/*to show whatever is done without the routing confusion*/}
-      {/* <Navbar/>
-      <Home/> */}
-      {/* <About/> */}
-
-      {/*codedamn*/}
         <BrowserRouter>
           <Navbar/>
           <Routes>
-            {/* <Route path="/" element={<Navbar/>}> */}
 
-              {/* <Route index element={<Home/>}/> */}
               <Route path="/Home" element={<Home />} />
               <Route path="/About" element={<About />} />
               <Route path="/Contact" element={<Contact />} />
               <Route path="/States" element={<States />} />
             
-            {/* </Route> */}
           </Routes>
         </BrowserRouter>
     </div>
   );
 }
-
-// const Root=() => {
-//   <>
-//     <div>
-//       <Link to="/">Home</Link>
-//       <Link to="/About">About Us</Link>
-//     </div>
-
-//     <div>
-//       <Outlet/>
-//     </div>
-//   </>
-// }
 
 export default App;
